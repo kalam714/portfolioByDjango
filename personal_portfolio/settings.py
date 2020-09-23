868f8ced5677b84fd16c9961b9d0e7fce27dd85f
kalam ahmed <kalamahmed714@gmail.com>
2020-09-23 16:02:55 +0600

    Added

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT= BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("you are in live")
+
+
